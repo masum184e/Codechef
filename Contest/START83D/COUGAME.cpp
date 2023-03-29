be12e932816e49ba797e0df5d093f9b375e0b2d4
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-03-30 00:08:30 +0600

    Initial Commit

---
+++ Contest/START83D/COUGAME.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  ll T;
+  cin>>T;
+  while(T--){
+    ll G,B;
+    cin>>G>>B;
+    cout<<B-G<<endl;
+  }
+  return 0;
+}
