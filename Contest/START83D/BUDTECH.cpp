8c6bd1b01e1c095fb860953f8fc75305d3b4de18
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-03-30 00:09:54 +0600

    Initial Commit

---
+++ Contest/START83D/BUDTECH.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  ll T;
+  cin>>T;
+  while(T--){
+    ll R;
+    cin>>R;
+    cout<<R*100<<endl;
+  }
+  return 0;
+}
