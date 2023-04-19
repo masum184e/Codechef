ab403adfc3be962f150fa91e4785f7818d691180
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-04-20 03:05:59 +0600

    Initial Commit

---
+++ Contest/START86/CCLEARN.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+    
+  ll N;
+  cin>>N;
+  cout<<2*N<<endl;
+    
+  return 0;
+}
