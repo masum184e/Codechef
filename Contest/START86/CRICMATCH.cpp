32d943dc70610fc18a5b78e1a84bc511c11ea056
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-04-20 03:08:48 +0600

    Initial Commit

---
+++ Contest/START86/CRICMATCH.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  ll T;
+  cin>>T;
+  while(T--){
+    ll N,M;
+    cin>>N>>M;
+    (N>M*6*6)?cout<<"NO"<<endl:cout<<"YES"<<endl;
+  }
+  return 0;
+}
