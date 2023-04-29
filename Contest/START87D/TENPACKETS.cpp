f3b4b82f0e6fc5b2fb48c3a9bfa9360b1763720d
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-04-29 21:17:15 +0600

    Initial Commit

---
+++ Contest/START87D/TENPACKETS.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  ll T;
+  cin>>T;
+  while(T--){
+    ll X,Y;
+    cin>>X>>Y;
+    cout<<(2*Y)+X<<endl;
+  }
+  return 0;
+}
