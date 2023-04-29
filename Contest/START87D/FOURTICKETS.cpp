56817d45e1a7248b2aea0e015db97505fb54f144
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-04-29 21:16:43 +0600

    Initial Commit

---
+++ Contest/START87D/FOURTICKETS.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  ll T;
+  cin>>T;
+  while(T--){
+    ll X;
+    cin>>X;
+    (X*4>1000)?cout<<"NO"<<endl:cout<<"YES"<<endl;
+  }
+  return 0;
+}
