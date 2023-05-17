6daa93cfc811866d2393734829bd545cd4d17344
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-05-17 20:33:54 +0600

    Initial Commit

---
+++ Contest/START90D/LEARNSQL.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  
+  ll R,C,E;
+  cin>>R>>C>>E;
+  cout<<(R+E)*C<<endl;
+  
+  return 0;
+}
