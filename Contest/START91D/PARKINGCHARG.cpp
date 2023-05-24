4a8c88e23384bc2aa8f8a7266dae0d1ea7992015
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-05-24 22:30:22 +0600

    Initial Commit

---
+++ Contest/START91D/PARKINGCHARG.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  ll T;
+
+   ll X,Y,H;
+   cin>>X>>Y>>H;
+   cout<<(H-1)*Y+X<<endl;
+  
+  return 0;
+}
