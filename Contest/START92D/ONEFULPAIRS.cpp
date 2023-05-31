51db1a08286a7be93df924521f62bd052b7c7a90
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-05-31 21:10:20 +0600

    Initial Commit

---
+++ Contest/START92D/ONEFULPAIRS.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+
+  ll a,b;
+  cin>>a>>b;
+  (a+b+(a*b)==111)?cout<<"Yes"<<endl:cout<<"No"<<endl;
+
+  return 0;
+}
