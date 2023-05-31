e7e05a4c4cb5c8b47c79049d937e39b1f4f4dea6
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-05-31 22:12:25 +0600

    Initial Commit

---
+++ Contest/START92D/RCT.cpp
@@ -0,0 +1,16 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  ll T;
+  cin>>T;
+  while(T--){
+    ll W,L,sum=0;
+    cin>>W>>L;
+    
+    (W%2!=0 && L%2!=0)?cout<<"NO"<<endl:cout<<"YES"<<endl;
+
+  }
+  return 0;
+}
