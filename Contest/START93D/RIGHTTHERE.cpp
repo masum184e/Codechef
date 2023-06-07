1a04db7df6f0fbba50d0b1fdcf938f6f9353134e
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-06-08 00:04:06 +0600

    Initial Commit

---
+++ Contest/START93D/RIGHTTHERE.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  ll T;
+  cin>>T;
+  while(T--){
+    ll N,X;
+    cin>>N>>X;
+    (X>=N)?cout<<"YES"<<endl:cout<<"NO"<<endl;
+  }
+  return 0;
+}
