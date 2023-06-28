c60e30a855cd6305b0e9e2bc1584195ccd39c3d0
Masum Billah <68304761+masum184e@users.noreply.github.com>
2023-06-28 21:48:03 +0600

    Initial Commit

---
+++ Contest/START96D/ABDIFF.cpp
@@ -0,0 +1,14 @@
+#include<bits/stdc++.h>
+using namespace std;
+#define ll long long int
+int main(){
+  ios_base::sync_with_stdio(false);cin.tie(NULL);
+  // ll T;
+  // cin>>T;
+  // while(T--){
+    ll A,B;
+    cin>>A>>B;
+    cout<<abs((A*B)-(A+B))<<endl;
+  // }
+  return 0;
+}
